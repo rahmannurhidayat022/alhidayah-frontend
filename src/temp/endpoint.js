287a357e9494d282e92c201698b51a93e10500ab
panji <panjiprasetyo025@gmail.com>
2022-12-08 15:32:32 +0700

    menampilkan data Artikel di halaman Landing Artikel

--- src/temp/endpoint.js
+++ src/temp/endpoint.js
@@ -1,3 +1,3 @@
-export const HOST = 'http://127.0.0.1:8000';
-export const PUBLIC_STORAGE = 'http://127.0.0.1:8000/storage/';
+export const HOST = "http://192.168.1.15:8000";
+export const PUBLIC_STORAGE = "http://192.168.1.15:8000/storage/";
 export const ENDPOINT = `${HOST}/api/`;
