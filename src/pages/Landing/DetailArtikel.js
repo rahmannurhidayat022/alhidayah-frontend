3d8ccd1975c904f1257d80f790194b80b0a8a34a
panji <panjiprasetyo025@gmail.com>
2022-12-12 17:08:57 +0700

    berhasil menampilkan data detail artikel

--- src/pages/Landing/DetailArtikel.js
+++ src/pages/Landing/DetailArtikel.js
@@ -1,54 +1,36 @@
-import { useEffect, useState } from 'react';
-import { BiTimeFive } from 'react-icons/bi';
-import { useParams, Navigate } from 'react-router-dom';
-import Breadcrumb from '../../components/UI/Breadcrumb';
-import { artikel as data } from '../../temp/data';
+import { useEffect } from "react";
+import { BiTimeFive } from "react-icons/bi";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import Breadcrumb from "../../components/UI/Breadcrumb";
+import { getArticleById } from "../../store/actions/article-action";
 
 const DetailArtikel = () => {
-	const [artikel, setArtikel] = useState({});
-	const { id } = useParams();
+  const { id } = useParams();
+  const { item } = useSelector((state) => state.article);
+  const dispatch = useDispatch();
 
-	const getArtikel = () => {
-		let filtered = data?.filter((item) => item.id === id);
-		filtered = filtered.length > 0 ? filtered[0] : null;
-		setArtikel(filtered);
-	};
+  const URL_STORAGE = process.env.REACT_APP_STORAGE;
 
-	useEffect(() => {
-		getArtikel();
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, []);
+  useEffect(() => {
+    dispatch(getArticleById(id));
+  }, [dispatch, id]);
 
-	if (artikel === null) {
-		return <Navigate to={'*'} replace />;
-	}
+  return (
+    <>
+      <Breadcrumb desc={{ link: "/artikel", title: "Kembali" }} title="Detail Artikel" />
 
-	return (
-		<>
-			<Breadcrumb
-				desc={{ link: '/artikel', title: 'Kembali' }}
-				title="Detail Artikel"
-			/>
-
-			<section className="w-11/12 py-6 md:w-[500px] lg:w-[700px] mx-auto">
-				<img
-					src={artikel?.image}
-					alt={`poto dari artikel ${artikel?.title}`}
-					className="w-full bg-cover"
-				/>
-				<h2 className="font-semibold text-2xl md:text-3xl mb-1">
-					{artikel?.title}
-				</h2>
-				<span className="flex items-center gap-1 text-16 text-gray-500">
-					<BiTimeFive size={20} />
-					{artikel?.createdAt}
-				</span>
-				<p className="mt-4 font-medium text-[16px] whitespace-pre-line break-words leading-relaxed">
-					{artikel?.body}
-				</p>
-			</section>
-		</>
-	);
+      <section className="w-11/12 py-6 md:w-[500px] lg:w-[700px] mx-auto">
+        <img src={`${URL_STORAGE}${item?.image}`} alt={`poto dari artikel ${item?.title}`} className="w-full bg-cover" />
+        <h2 className="font-semibold text-2xl md:text-3xl mb-1">{item?.title}</h2>
+        <span className="flex items-center gap-1 text-16 text-gray-500">
+          <BiTimeFive size={20} />
+          {item?.created_at}
+        </span>
+        <p className="mt-4 font-medium text-[16px] whitespace-pre-line break-words leading-relaxed" dangerouslySetInnerHTML={{ __html: item?.desc }}></p>
+      </section>
+    </>
+  );
 };
 
 export default DetailArtikel;
