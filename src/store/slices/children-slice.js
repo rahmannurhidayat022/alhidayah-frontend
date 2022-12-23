416880d2e0c521ff1a97fbc0d7d6857e06b33943
rahmannurhidayat022 <rahmannurhidayat022@gmail.com>
2022-12-23 14:32:30 +0700

    add reducer to exraslice children

--- src/store/slices/children-slice.js
+++ src/store/slices/children-slice.js
@@ -1,4 +1,11 @@
 import { createSlice } from "@reduxjs/toolkit";
+import {
+  addChildrenData,
+  deleteChildrenDataById,
+  getAllChildrenData,
+  getChildrenDataById,
+  updateChildrenDataById,
+} from "../actions/children-action";
 
 const initialState = {
   items: null,
@@ -22,7 +29,81 @@ const childrenSlice = createSlice({
   name: "children",
   initialState,
   reducers: {},
-  extraReducers: {},
+  extraReducers: {
+    [getAllChildrenData.pending]: (state) => {
+      state.loading = true;
+      state.success = null;
+      state.error = null;
+    },
+    [getAllChildrenData.fulfilled]: (state, { payload }) => {
+      state.loading = false;
+      state.items = payload.data;
+      state.pagination.firstPage = payload.first_page_url;
+      state.pagination.lastPage = payload.last_page_url;
+      state.pagination.next = payload.next_page_url;
+      state.pagination.prev = payload.prev_page_url;
+      state.pagination.links = payload.links;
+      state.pagination.totalItem = payload.total;
+      state.pagination.totalPage = payload.last_page;
+      state.pagination.currentPage = payload.current_page;
+    },
+    [getAllChildrenData.rejected]: (state, { payload }) => {
+      state.loading = false;
+      state.error = payload;
+    },
+    [getChildrenDataById.pending]: (state) => {
+      state.loading = true;
+      state.success = null;
+      state.error = null;
+    },
+    [getChildrenDataById.fulfilled]: (state, { payload }) => {
+      state.loading = false;
+      state.item = payload;
+    },
+    [getChildrenDataById.rejected]: (state, { payload }) => {
+      state.loading = false;
+      state.error = payload;
+    },
+    [updateChildrenDataById.pending]: (state) => {
+      state.loading = true;
+      state.success = null;
+      state.error = null;
+    },
+    [updateChildrenDataById.fulfilled]: (state, { payload }) => {
+      state.loading = false;
+      state.success = payload;
+    },
+    [updateChildrenDataById.rejected]: (state, { payload }) => {
+      state.loading = false;
+      state.error = payload;
+    },
+    [addChildrenData.pending]: (state) => {
+      state.loading = true;
+      state.success = null;
+      state.error = null;
+    },
+    [addChildrenData.fulfilled]: (state, { payload }) => {
+      state.loading = false;
+      state.success = payload;
+    },
+    [addChildrenData.rejected]: (state, { payload }) => {
+      state.loading = false;
+      state.error = payload;
+    },
+    [deleteChildrenDataById.pending]: (state) => {
+      state.loading = true;
+      state.success = null;
+      state.error = null;
+    },
+    [deleteChildrenDataById.fulfilled]: (state, { payload }) => {
+      state.loading = false;
+      state.success = payload;
+    },
+    [deleteChildrenDataById.rejected]: (state, { payload }) => {
+      state.loading = false;
+      state.error = payload;
+    },
+  },
 });
 
 export default childrenSlice.reducer;
