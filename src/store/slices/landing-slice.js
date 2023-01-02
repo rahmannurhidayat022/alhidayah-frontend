ec16495d4822b7d63dfee00a4174068383d9a900
panji <panjiprasetyo025@gmail.com>
2023-01-02 16:45:45 +0700

    render data halaman kontak

--- src/store/slices/landing-slice.js
+++ src/store/slices/landing-slice.js
@@ -1,95 +1,102 @@
 import { createSlice } from "@reduxjs/toolkit";
-import {
-	getHomeData,
-	getInstitutionProfile,
-	getTelpNumber,
-	getVisiMisiData,
-	searchDonationHistory,
-} from "../actions/landing-action";
+import { getContactPageData, getHomeData, getInstitutionProfile, getTelpNumber, getVisiMisiData, searchDonationHistory } from "../actions/landing-action";
 
 const initialState = {
-	articles: null,
-	galleries: null,
-	telphone: null,
-	items: null,
-	item: null,
-	loading: false,
-	success: null,
-	error: null,
+  articles: null,
+  galleries: null,
+  telphone: null,
+  items: null,
+  item: null,
+  loading: false,
+  success: null,
+  error: null,
 };
 
 const landingSlice = createSlice({
-	name: "landing",
-	initialState,
-	reducers: {},
-	extraReducers: {
-		[getHomeData.pending]: (state) => {
-			state.loading = true;
-			state.success = null;
-			state.error = null;
-		},
-		[getHomeData.fulfilled]: (state, { payload }) => {
-			state.loading = false;
-			state.articles = payload?.articles;
-			state.galleries = payload?.galleries;
-		},
-		[getHomeData.rejected]: (state, { payload }) => {
-			state.loading = false;
-			state.erorr = payload;
-		},
-		[getTelpNumber.pending]: (state) => {
-			state.loading = true;
-			state.success = null;
-			state.error = null;
-		},
-		[getTelpNumber.fulfilled]: (state, { payload }) => {
-			state.loading = false;
-			state.telphone = payload?.no_telp;
-		},
-		[getTelpNumber.rejected]: (state, { payload }) => {
-			state.loading = false;
-			state.error = payload;
-		},
-		[getInstitutionProfile.pending]: (state) => {
-			state.loading = true;
-			state.success = null;
-			state.error = null;
-		},
-		[getInstitutionProfile.fulfilled]: (state, { payload }) => {
-			state.loading = false;
-			state.item = payload;
-		},
-		[getInstitutionProfile.rejected]: (state, { payload }) => {
-			state.loading = false;
-			state.error = payload;
-		},
-		[getVisiMisiData.pending]: (state) => {
-			state.loading = true;
-			state.success = null;
-			state.error = null;
-		},
-		[getVisiMisiData.fulfilled]: (state, { payload }) => {
-			state.loading = false;
-			state.item = payload;
-		},
-		[getVisiMisiData.rejected]: (state, { payload }) => {
-			state.loading = false;
-			state.error = payload;
-		},
-		[searchDonationHistory.pending]: (state) => {
-			state.loading = true;
-			state.success = null;
-			state.error = null;
-		},
-		[searchDonationHistory.fulfilled]: (state, { payload }) => {
-			state.loading = false;
-			state.items = payload;
-		},
-		[searchDonationHistory.rejected]: (state, { payload }) => {
-			state.loading = false;
-			state.error = payload;
-		},
-	},
+  name: "landing",
+  initialState,
+  reducers: {},
+  extraReducers: {
+    [getHomeData.pending]: (state) => {
+      state.loading = true;
+      state.success = null;
+      state.error = null;
+    },
+    [getHomeData.fulfilled]: (state, { payload }) => {
+      state.loading = false;
+      state.articles = payload?.articles;
+      state.galleries = payload?.galleries;
+    },
+    [getHomeData.rejected]: (state, { payload }) => {
+      state.loading = false;
+      state.erorr = payload;
+    },
+    [getTelpNumber.pending]: (state) => {
+      state.loading = true;
+      state.success = null;
+      state.error = null;
+    },
+    [getTelpNumber.fulfilled]: (state, { payload }) => {
+      state.loading = false;
+      state.telphone = payload?.no_telp;
+    },
+    [getTelpNumber.rejected]: (state, { payload }) => {
+      state.loading = false;
+      state.error = payload;
+    },
+    [getInstitutionProfile.pending]: (state) => {
+      state.loading = true;
+      state.success = null;
+      state.error = null;
+    },
+    [getInstitutionProfile.fulfilled]: (state, { payload }) => {
+      state.loading = false;
+      state.item = payload;
+    },
+    [getInstitutionProfile.rejected]: (state, { payload }) => {
+      state.loading = false;
+      state.error = payload;
+    },
+    [getVisiMisiData.pending]: (state) => {
+      state.loading = true;
+      state.success = null;
+      state.error = null;
+    },
+    [getVisiMisiData.fulfilled]: (state, { payload }) => {
+      state.loading = false;
+      state.item = payload;
+    },
+    [getVisiMisiData.rejected]: (state, { payload }) => {
+      state.loading = false;
+      state.error = payload;
+    },
+    [searchDonationHistory.pending]: (state) => {
+      state.loading = true;
+      state.success = null;
+      state.error = null;
+    },
+    [searchDonationHistory.fulfilled]: (state, { payload }) => {
+      state.loading = false;
+      state.items = payload;
+    },
+    [searchDonationHistory.rejected]: (state, { payload }) => {
+      state.loading = false;
+      state.error = payload;
+    },
+    [getContactPageData.pending]: (state) => {
+      state.loading = true;
+      state.success = null;
+      state.error = null;
+    },
+    [getContactPageData.fulfilled]: (state, { payload }) => {
+      state.loading = false;
+      state.item = payload;
+    },
+    [getContactPageData.rejected]: (state, { payload }) => {
+      state.loading = false;
+      state.error = payload;
+    },
+  },
 });
 
 export default landingSlice.reducer;
