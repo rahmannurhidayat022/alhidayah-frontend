d488e63ec80eb13e3bf4729529d3cbf0b2fcb636
rahmannurhidayat022 <rahmannurhidayat022@gmail.com>
2023-01-11 20:19:06 +0700

    removed qr code

--- src/pages/Landing/Rekening.js
+++ src/pages/Landing/Rekening.js
@@ -4,6 +4,7 @@ import { getAllDebit } from "../../store/actions/debit-action";
 import { useDispatch } from "react-redux";
 import Breadcrumb from "../../components/UI/Breadcrumb";
 import CopyToClipboard from "../../components/UI/CopyToClipboard";
+import { BsCreditCard2Front } from "react-icons/bs";
 
 const Rekening = () => {
   const { items } = useSelector((state) => state.debit);
@@ -13,28 +14,31 @@ const Rekening = () => {
     dispatch(getAllDebit());
   }, [dispatch]);
 
-  const renderData = items?.map(({ nomor_rekening, nama_bank, atas_nama }, index) => {
-    return (
-      <li key={index} className="mb-2">
-        <div className="flex flex-row gap-4">
-          <span className="font-semibold">Bank {nama_bank}:</span>
-          <CopyToClipboard value={nomor_rekening} />
-        </div>
-        <span>a.n. {atas_nama}</span>
-      </li>
-    );
-  });
+  const renderData = items?.map(
+    ({ nomor_rekening, nama_bank, atas_nama }, index) => {
+      return (
+        <li key={index} className="mb-2 flex flex-row gap-2">
+          <div className="">
+            <BsCreditCard2Front size={60} />
+          </div>
+          <div className="flex flex-col gap-1">
+            <span className="font-semibold">Bank {nama_bank}</span>
+            <CopyToClipboard className="flex-shrink" value={nomor_rekening} />
+            <span>a.n. {atas_nama}</span>
+          </div>
+        </li>
+      );
+    }
+  );
   return (
     <>
       <Breadcrumb title="Rekening Donasi" />
-      <section className="container-custom my-6 grid gap-6 grid-cols-1 md:grid-cols-2 md:gap-2">
-        <div>
-          <h2 className="uppercase text-lg font-semibold mb-4 text-center">SCAN QR</h2>
-          <img src="/images/QRNEW.png" alt="tentang yayasan alhidayah baitul hatim" className="md:w-200 lg:w-[300px] flex flex-col justify-center mx-auto " />
-        </div>
+      <section className="container-custom my-2 gap-1 grid-cols-1">
         <div>
           <div className="mb-4">
-            <h2 className="text-lg font-semibold mb-8 mt-10">Rekening Donasi Yayasan Al-Hidayah Baitul Hatim:</h2>
+            <h2 className="text-lg font-semibold mb-8 mt-10">
+              Rekening Donasi Yayasan Al-Hidayah Baitul Hatim
+            </h2>
             <ul className="">{renderData}</ul>
           </div>
         </div>
